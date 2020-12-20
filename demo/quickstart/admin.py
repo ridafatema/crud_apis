d7c71cc8fb2b37ccaa67d328f012e8d1a9abd1a6
Rida97 <ridafatema11@gmail.com>
2020-12-20 13:57:41 +0500

    initial commit

--- demo/quickstart/admin.py
+++ demo/quickstart/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from quickstart.models import User, Task
+#from quickstart.models import User, Task
 
-admin.site.register(User)
-admin.site.register(Task)
+#admin.site.register(User)
+#admin.site.register(Task)
